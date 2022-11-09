e199642f60a5b8ba02b6c61a64a81fa09a78c47b
CosminBozai <cosminbozai@outlook.com>
2022-11-09 11:22:55 +0000

    Added state for logged status

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import "./App.css";
-import Header from "./components/Header";
-import FriendList from "./components/FriendList";
-import ChatContainer from "./components/ChatContainer";
+import Header from "./containers/Header/Header";
+import FriendList from "./containers/FriendList";
+import ChatContainer from "./containers/ChatContainer";
 
 function App() {
+  const [logStatus, setLogStatus] = useState(false);
   return (
     <div id="app-container">
-      <Header />
+      <Header logStatus={logStatus} setLogStatus={setLogStatus} />
       <FriendList />
       <ChatContainer />
     </div>

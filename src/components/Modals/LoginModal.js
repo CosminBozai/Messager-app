1dcea5380f90e0b5507a0e26cbdb26f87062720a
CosminBozai <cosminbozai@outlook.com>
2022-11-10 14:24:21 +0000

    Login user with email and password

--- src/components/Modals/LoginModal.js
+++ src/components/Modals/LoginModal.js
@@ -1,26 +1,46 @@
 import React, { useState } from "react";
 import SignupModal from "./SignupModal";
+import { auth, signInWithEmailAndPassword } from "../../firebase";
 import "./Modal.css";
 
-export default function LoginModal({ setShowLogin }) {
+export default function LoginModal() {
   const [showSignup, setShowSignup] = useState(false);
-
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  async function loginUser() {
+    await signInWithEmailAndPassword(auth, email, password);
+  }
   return (
     <>
       <div className="modal login">
         <form id="login-form">
           <div className="input-field">
             <label htmlFor="login-email">Email address</label>
-            <input type="email" id="login-email" required="" />
+            <input
+              type="email"
+              id="login-email"
+              required=""
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
           </div>
           <div className="input-field">
             <label htmlFor="login-password">Your password</label>
-            <input type="password" id="login-password" required="" />
+            <input
+              type="password"
+              id="login-password"
+              required=""
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
           </div>
           <button
             type="button"
             className="form-btn login-btn"
             data-testid="modal-login-btn"
+            onClick={loginUser}
           >
             Log in
           </button>

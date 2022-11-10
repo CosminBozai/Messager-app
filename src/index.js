6f317184f00579e17c6890db2692b76b80662173
CosminBozai <cosminbozai@outlook.com>
2022-11-10 12:35:23 +0000

    Moved firebase init in another module

--- src/index.js
+++ src/index.js
@@ -2,19 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { initializeApp } from "firebase/app";
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDPNXff-fw9QklJjOOm0uJyC2Wt9Ol-iZU",
-  authDomain: "messager-app-c6790.firebaseapp.com",
-  projectId: "messager-app-c6790",
-  storageBucket: "messager-app-c6790.appspot.com",
-  messagingSenderId: "17673740631",
-  appId: "1:17673740631:web:83fd9d14cd81378db3de12",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

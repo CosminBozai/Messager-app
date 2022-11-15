7a886343506275cac53a3493bbc1019b0be1f79e
CosminBozai <cosminbozai@outlook.com>
2022-11-15 14:26:50 +0000

    Upload custom profile photo

---
+++ src/components/Modals/IconModal.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { useState } from "react";
+import { auth } from "../../firebase/auth";
+import { useEffect, useRef } from "react";
+import { storage, ref, uploadBytes } from "../../firebase/storage";
+
+export default function IconModal() {
+  const currentIcon = document
+    .querySelector("#profile-icon")
+    .getAttribute("src");
+  const [iconPrev, setIconPrev] = useState(currentIcon);
+  const [icon, setIcon] = useState(null);
+  const user = auth.currentUser;
+  const inputFile = useRef(null);
+  function selectIcon() {
+    // `current` points to the mounted file input element
+    inputFile.current.click();
+  }
+  // Display preview of the new icon
+  function handleChange(e) {
+    setIconPrev(URL.createObjectURL(e.target.files[0]));
+    setIcon(e.target.files[0]);
+  }
+  // Upload the image to the storage belonging to the current user
+  function uploadImage() {
+    const storageRef = ref(storage, `${user.uid}/icon`);
+    uploadBytes(storageRef, icon);
+  }
+  return (
+    <div className="modal icon">
+      <div className="container">
+        <div className="icon-container">
+          <img src={iconPrev} alt="#" />
+        </div>
+        <button onClick={selectIcon}> Select icon </button>
+        <button onClick={uploadImage}>Submit</button>
+        <input
+          onChange={handleChange}
+          type="file"
+          id="file"
+          ref={inputFile}
+          style={{ display: "none" }}
+        />
+      </div>
+    </div>
+  );
+}
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 import { firestore, doc, getDoc } from "../../firebase/firestore";
 import { auth } from "../../firebase/auth";
 import { storage, ref, getDownloadURL } from "../../firebase/storage";
+import IconModal from "../Modals/IconModal";
 import "./User.css";
 
 export default function User() {
   // State that contains the info about the current logged user
   const [userData, setUserData] = useState({});
+  const [showIconModal, setShowIconModal] = useState(false);
   const user = auth.currentUser;
   // Get the user data from the database when the component mounts
   useEffect(() => {
@@ -24,8 +26,14 @@ export default function User() {
       {" "}
       <p className="username">{userData.username}</p>
       <div data-testid="profile-icon" className="user-icon">
-        <img src={userData.iconURL} alt="#"></img>
+        <img
+          onClick={() => setShowIconModal(true)}
+          id="profile-icon"
+          src={userData.iconURL}
+          alt="#"
+        ></img>
       </div>
+      {showIconModal && <IconModal />}
     </>
   );
 }

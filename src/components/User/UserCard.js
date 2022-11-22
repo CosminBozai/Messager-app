3da9ed7723d5a7a802b4bc8c166e3de4a42d7358
CosminBozai <cosminbozai@outlook.com>
2022-11-22 20:11:50 +0000

    Render the friends's username and icon

--- src/components/User/UserCard.js
+++ src/components/User/UserCard.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function UserCard() {
-  return <li className="bg-black h-16 w-full"></li>;
+export default function UserCard({ username, icon }) {
+  return (
+    <li className="h-16 w-full border border-black">
+      <img
+        className="inline-block h-8 w-8 rounded-full"
+        src={icon}
+        alt="#"
+      ></img>
+      <p className="inline">{username}</p>
+    </li>
+  );
 }
--- src/containers/FriendList.js
+++ src/containers/FriendList.js
@@ -1,9 +1,35 @@
 import UserCard from "../components/User/UserCard";
+import React, { useEffect, useState } from "react";
+import { auth } from "../firebase/auth";
+import {
+  firestore,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "../firebase/firestore";
 
 export default function FriendList() {
+  const [friends, setFriends] = useState([]);
+  useEffect(() => {
+    const user = auth.currentUser;
+    async function getData() {
+      const q = query(
+        collection(firestore, "users"),
+        where("username", "!=", user.displayName)
+      );
+      const querySnap = await getDocs(q);
+      querySnap.forEach((doc) => {
+        setFriends((current) => [...current, doc.data()]);
+      });
+    }
+    getData();
+  }, []);
   return (
     <ul className="w-64 h-[90%] bg-gray-100 border-2" id="friend-list">
-      <UserCard />
+      {friends.map((friend) => {
+        return <UserCard username={friend.username} icon={friend.icon} />;
+      })}
     </ul>
   );
 }

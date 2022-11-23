58594ef1955272e5137cb53ff227523650cf3d86
CosminBozai <cosminbozai@outlook.com>
2022-11-23 12:40:17 +0000

    Changed the logic for getting the user

--- src/containers/FriendList.js
+++ src/containers/FriendList.js
@@ -1,6 +1,6 @@
 import UserCard from "../components/User/UserCard";
 import React, { useEffect, useState } from "react";
-import { auth } from "../firebase/auth";
+import { auth, onAuthStateChanged } from "../firebase/auth";
 import {
   firestore,
   collection,
@@ -12,23 +12,27 @@ import {
 export default function FriendList() {
   const [friends, setFriends] = useState([]);
   useEffect(() => {
-    const user = auth.currentUser;
-    async function getData() {
-      const q = query(
-        collection(firestore, "users"),
-        where("username", "!=", user.displayName)
-      );
-      const querySnap = await getDocs(q);
-      querySnap.forEach((doc) => {
-        setFriends((current) => [...current, doc.data()]);
-      });
-    }
-    getData();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const q = query(
+          collection(firestore, "users"),
+          where("uid", "!=", user.uid)
+        );
+        getDocs(q).then((querySnap) => {
+          querySnap.forEach((doc) => {
+            setFriends((current) => [...current, doc.data()]);
+          });
+        });
+      } else return;
+    });
   }, []);
+
   return (
     <ul className="w-64 h-[90%] bg-gray-100 border-2" id="friend-list">
-      {friends.map((friend) => {
-        return <UserCard username={friend.username} icon={friend.icon} />;
+      {friends.map((friend, i) => {
+        return (
+          <UserCard key={i} username={friend.username} icon={friend.icon} />
+        );
       })}
     </ul>
   );

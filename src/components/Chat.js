ffe497fe68a35d30cd6640f72b2e82ed2df8c23d
CosminBozai <cosminbozai@outlook.com>
2022-11-24 12:43:53 +0000

    Chat component

---
+++ src/components/Chat.js
@@ -0,0 +1,3 @@
+export default function Chat() {
+  return <div className="h-5/6"></div>;
+}
--- src/containers/ChatContainer.js
+++ src/containers/ChatContainer.js
@@ -1,7 +1,9 @@
+import Chat from "../components/Chat";
+
 export default function ChatContainer() {
   return (
     <div className="flex-auto" id="chat-container">
-      <div className="h-5/6"></div>
+      <Chat />
       <div className="h-1/6 flex items-center justify-around border-t-2 shadow-md">
         <input className="w-80 h-10 border border-black" type="text"></input>
         <button className="bg-sky-600 p-2 text-white" type="button">

fccd462f62a8860691339675c492cc5dd0c1abc1
CosminBozai <cosminbozai@outlook.com>
2022-11-24 12:21:20 +0000

    Added input and send button

--- src/containers/ChatContainer.js
+++ src/containers/ChatContainer.js
@@ -1,3 +1,13 @@
 export default function ChatContainer() {
-  return <div id="chat-container"></div>;
+  return (
+    <div className="flex-auto" id="chat-container">
+      <div className="h-5/6"></div>
+      <div className="h-1/6 flex items-center justify-around border-t-2 shadow-md">
+        <input className="w-80 h-10 border border-black" type="text"></input>
+        <button className="bg-sky-600 p-2 text-white" type="button">
+          Send
+        </button>
+      </div>
+    </div>
+  );
 }

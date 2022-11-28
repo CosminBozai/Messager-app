3ac42bfd8e7b9919a28b9d37b604de348954f79e
CosminBozai <cosminbozai@outlook.com>
2022-11-28 18:05:15 +0000

    Style for the "No message" para

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -16,7 +16,8 @@ export default function Chat() {
       setMessages((prev) => [
         ...prev,
         {
-          text: `No message with ${activeFriend.username}`,
+          text: `No message with ${activeFriend.username}. Be the first to send a message!`,
+          sender: "noMsg",
         },
       ]);
     }
@@ -31,16 +32,22 @@ export default function Chat() {
         if (msg.sender === user.uid) {
           return (
             <p
-              className="block mb-1 px-2 py-1 align-baseline w-max bg-slate-200 rounded-lg text-black"
+              className="block max-w-[55ch] mb-1 ml-[100%] -translate-x-[100%] px-2 py-1 align-baseline w-max bg-slate-200 rounded-lg text-black"
               key={i}
             >
+              {msg.text}
+            </p>
+          );
+        } else if (msg.sender === "noMsg") {
+          return (
+            <p className="block m-auto w-max text-lg" key={i}>
               {msg.text}
             </p>
           );
         } else {
           return (
             <p
-              className="block mb-1 px-2 py-1 align-baseline w-max bg-sky-600 rounded-lg text-white"
+              className="block max-w-[55ch] mb-1 px-2 py-1 align-baseline w-max bg-sky-600 rounded-lg text-white"
               key={i}
             >
               {msg.text}
